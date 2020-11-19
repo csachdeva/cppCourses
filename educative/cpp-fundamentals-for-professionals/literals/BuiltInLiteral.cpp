c8fa9fc0078fc47b16ebe5a964f4380515899d7f
csachdeva <chetansachdeva2010@gmail.com>
2020-11-19 22:35:38 +0530

    Basic literal

---
+++ educative/cpp-fundamentals-for-professionals/literals/BuiltInLiteral.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    int unsigned_int = u'U'; // Unsigned integer
+    int hex = 0x2a; // Hexadecimal
+    std::string raw = R"(Raw String)"; // Raw string
+    std::cout << unsigned_int << ", " << hex << ", " << raw << "\n";
+}
